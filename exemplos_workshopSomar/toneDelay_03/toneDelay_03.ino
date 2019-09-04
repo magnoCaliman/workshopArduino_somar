bf74ef8674e84d286276deb799e6bc8672378cbd
mC_ <m@g.no>
2019-09-04 19:27:56 +0100

    commit final

--- exemplos_workshopSomar/toneDelay_03/toneDelay_03.ino
+++ exemplos_workshopSomar/toneDelay_03/toneDelay_03.ino
@@ -5,6 +5,8 @@ void setup()
 
 void loop()
 {
-  tone(5, 100, 500); //agora com 3 argumentos
-  delay(1000);
+  tone(5, 500, 500);
+  delay(500);
+  tone(5, 500, 500);
+  delay(500);
 }
--- exemplos_workshopSomar/tone_02/tone_02.ino
+++ exemplos_workshopSomar/tone_02/tone_02.ino
@@ -6,4 +6,5 @@ void setup()
 void loop()
 {
   tone(5, 100);
+  tone(5, 101);
 }
